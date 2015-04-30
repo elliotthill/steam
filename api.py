d078672727e205e64b1b456ec3abddb5031cabda
Elliott Hill <elliott.hill@gmail.com>
2015-04-30 14:33:25 +0100

    Added name attire

--- api.py
+++ api.py
@@ -16,10 +16,12 @@ class SteamGame:
         payload = {'appids':self.appid, 'cc': self.country, 'l': 'english', 'v': 1}
 
         #Spoof user agent
-        user_agent = {'User-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.125 Safari/537.36',
+        user_agent = {
+                      'User-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.125 Safari/537.36',
                       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
                       'Accept-language': 'en-US,en;q=0.8',
-                      'Accept-Encoding': 'gzip,deflate,sdch'}
+                      'Accept-Encoding': 'gzip,deflate,sdch'
+        }
 
 
         r = requests.get(url, headers=user_agent, params=payload)
@@ -28,12 +30,16 @@ class SteamGame:
         try:
             self.price = float(j[self.appid]['data']['price_overview']['final'])/100.00
             self.price = round(self.price, 2)
+            self.name = j[self.appid]['data']['name']
         except ValueError:
             self.price = None
             print 'Steam Error: response does not contain a valid price'
         except KeyError:
             self.price = None
             print 'Steam Error: response was not valid'
+
+    def get_name(self):
+        return self.name
 
     def get_price(self):
         return self.price
